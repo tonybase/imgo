14e15398a41586fd520a0836f35c6cf420689b9d
ITNIK <997585659@qq.com>
2015-04-23 12:21:47 +0800

    添加注释

--- im/client.go
+++ im/client.go
@@ -10,35 +10,49 @@ import (
 客户端结构体
 */
 type Client struct {
-	name     string //客户端连接的唯一性 FIXME 需要修改成KEY name太笼统不能体现唯一性的概念
-	conn     net.Conn
-	incoming InMessage
-	outgoing OutMessage
-	reader   *bufio.Reader
-	writer   *bufio.Writer
-	quiting  chan *Client
+	name     string        //客户端连接的唯一性 FIXME 需要修改成KEY name太笼统不能体现唯一性的概念
+	conn     net.Conn      //连接
+	incoming InMessage     //输入消息
+	outgoing OutMessage    //输出消息
+	reader   *bufio.Reader //读取
+	writer   *bufio.Writer //输出
+	quiting  chan *Client  //退出
 }
 
+/*
+获取客户端名称
+*/
 func (this *Client) GetName() string {
 	return this.name
 }
 
+/*
+设置客户端名称
+*/
 func (this *Client) SetName(name string) {
 	this.name = name
 }
 
+/*
+获取输入消息
+*/
 func (this *Client) GetIncoming() IMRequest {
 	return <-this.incoming
 }
 
+/*
+设置输出消息
+*/
 func (this *Client) PutOutgoing(resp *IMResponse) {
 	this.outgoing <- *resp
 }
 
+/*
+创建客户端
+*/
 func CreateClient(name string, conn net.Conn) *Client {
 	reader := bufio.NewReader(conn)
 	writer := bufio.NewWriter(conn)
-
 	client := &Client{
 		name:     name,
 		conn:     conn,
@@ -52,22 +66,31 @@ func CreateClient(name string, conn net.Conn) *Client {
 	return client
 }
 
-// 自动读入或者写出消息
+/*
+自动读入或者写出消息
+*/
 func (this *Client) Listen() {
 	go this.read()
 	go this.write()
 }
 
-// 退出了一个连接
+/*
+退出了一个连接
+*/
 func (this *Client) Quit() {
 	this.quiting <- this
 }
 
-// 关闭连接通道
+/*
+关闭连接通道
+*/
 func (this *Client) Close() {
 	this.conn.Close()
 }
 
+/*
+读取消息
+*/
 func (this *Client) read() {
 	for {
 		if line, _, err := this.reader.ReadLine(); err == nil {
@@ -87,6 +110,9 @@ func (this *Client) read() {
 	}
 }
 
+/*
+输出消息
+*/
 func (this *Client) write() {
 	for resp := range this.outgoing {
 		if _, err := this.writer.WriteString(string(resp.Encode()) + "\n"); err != nil {
--- im/constants.go
+++ im/constants.go
@@ -1,23 +1,17 @@
 package im
 
 const (
-	GET_KEY_RETURN = "GET_KEY_RETURN" // 请求TCP获取连接KEY
-
-	GET_CONN        = "GET_CONN" // 建立TCP长连接
-	GET_CONN_RETURN = "GET_CONN_RETURN"
-
-	GET_BUDDY_LIST        = "GET_BUDDY_LIST" // 获取好友分组列表
-	GET_BUDDY_LIST_RETURN = "GET_BUDDY_LIST_RETURN"
-
-	CREATE_SESSION        = "CREATE_SESSION" // 创建会话
-	CREATE_SESSION_RETURN = "CREATE_SESSION_RETURN"
-
-	SEND_MSG        = "SEND_MSG"        // 发送消息
-	SEND_MSG_RETURN = "SEND_MSG_RETURN" // 发送消息
-	PUSH_MSG        = "PUSH_MSG"        // 接收消息
-
-	SEND_STATUS_CHANGE = "SEND_STATUS_CHANGE" // 发送状态
-	PUSH_STATUS_CHANGE = "PUSH_STATUS_CHANGE" // 接收状态
-
-	LOGOUT_REQUEST = "LOGOUT_REQUEST" // 退出
+	GET_KEY_RETURN        = "GET_KEY_RETURN"        // 请求TCP获取连接KEY
+	GET_CONN              = "GET_CONN"              // 建立TCP长连接
+	GET_CONN_RETURN       = "GET_CONN_RETURN"       // 获取连接返回
+	GET_BUDDY_LIST        = "GET_BUDDY_LIST"        // 获取好友列表
+	GET_BUDDY_LIST_RETURN = "GET_BUDDY_LIST_RETURN" // 获取好友列表返回
+	CREATE_SESSION        = "CREATE_SESSION"        // 创建会话
+	CREATE_SESSION_RETURN = "CREATE_SESSION_RETURN" // 创建会话返回
+	SEND_MSG              = "SEND_MSG"              // 发送消息
+	SEND_MSG_RETURN       = "SEND_MSG_RETURN"       // 发送消息返回
+	PUSH_MSG              = "PUSH_MSG"              // 接收消息
+	SEND_STATUS_CHANGE    = "SEND_STATUS_CHANGE"    // 发送状态
+	PUSH_STATUS_CHANGE    = "PUSH_STATUS_CHANGE"    // 接收状态
+	LOGOUT_REQUEST        = "LOGOUT_REQUEST"        // 退出
 )
