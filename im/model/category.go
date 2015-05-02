d6c3fc5843bb0dfcac712b21c2f276d32ac580da
Tony <tony@ganguo.hk>
2015-05-02 22:38:28 +0800

    修改调整

--- im/model/category.go
+++ im/model/category.go
@@ -22,26 +22,26 @@ func (this *Group) Encode() []byte {
 }
 
 /*
-解析JSON数据
-*/
+ 解析JSON数据
+ */
 func (this *Group) Decode(data []byte) error {
 	err := json.Unmarshal(data, this)
 	return err
 }
 
 /*
-分组PUSH用户(仅传递数据 非操作数据库方法)
-*/
+ 分组PUSH用户(仅传递数据 非操作数据库方法)
+ */
 func (this *Group) AddUser(u IMUser) {
 	this.Buddies = append(this.Buddies, u)
 }
 
 /*
-根据token获取分组数据
-*/
+ 根据token获取分组数据
+ */
 func GetGroupsByToken(token string) []Group {
 	var groups []Group
-	rows, _ := Database.Query("select g.id,g.name from im_group g left join im_login l on l.user_id=g.creater where token=?", token)
+	rows, _ := Database.Query("select g.id, g.name from im_group g left join im_login l on l.user_id=g.creator where token=?", token)
 	defer rows.Close()
 	for rows.Next() {
 		var group Group
