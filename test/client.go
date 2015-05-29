7b9f57a44108ff3e534267252536cd82ed904440
ITNIK <997585659@qq.com>
2015-05-29 12:48:18 +0800

    压测代码(测试未通过--操他大爷的)

--- test/client.go
+++ test/client.go
@@ -1,68 +1,98 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net"
-	"encoding/json"
-
 )
 
-const (
-	tcpaddr = "127.0.0.1:9090"
-)
+type Clienter struct {
+	client  net.Conn
+	isAlive bool
+	SendStr chan string
+	RecvStr chan string
+}
 type IMResponse struct {
-	Status int         `json:"status"` //状态 0成功，非0错误
-	Msg    string      `json:"msg"`    //消息
+	Status int                               `json:"status"` //状态 0成功，非0错误
+	Msg    string                            `json:"msg"`    //消息
 	Data   map[string]map[string]interface{} `json:"data"`   //数据
-	Refer  string      `json:"refer"`  //来源
-}
-func main() {
-
-	Client()
-	/**for i:=0;i<3 ;i++  {
-		 Client()
-	}**/
-
+	Refer  string                            `json:"refer"`  //来源
 }
 
-func Client() {
-	conn, err := net.Dial("tcp", tcpaddr)
-	defer conn.Close()
+func (c *Clienter) Connect() bool {
+	if c.isAlive {
+		return true
+	} else {
+		var err error
+		c.client, err = net.Dial("tcp", "127.0.0.1:9090")
+		if err != nil {
+			fmt.Printf("Failure to connet:%s\n", err.Error())
+			return false
+		}
+		c.isAlive = true
+	}
+	return true
+}
+
+func (c *Clienter) Echo() {
+	line := <-c.SendStr
+	c.client.Write([]byte(line))
+	buf := make([]byte, 1024)
+	n, err := c.client.Read(buf)
 	if err != nil {
-		fmt.Println("连接服务端失败:", err.Error())
+		c.RecvStr <- string("Server close...")
+		c.client.Close()
+		c.isAlive = false
 		return
 	}
-	fmt.Println("已连接服务器")
-	sms := make([]byte, 1024)
-	sms=[]byte("")
+	c.RecvStr <- string(buf[0:n])
+}
+
+func Work(tc *Clienter) {
+	if !tc.isAlive {
+		if tc.Connect() {
+			tc.Echo()
+		} else {
+			<-tc.SendStr
+			tc.RecvStr <- string("Server close...")
+		}
+	} else {
+		tc.Echo()
+	}
+}
+func main() {
+	var tc Clienter
+	tc.SendStr = make(chan string)
+	tc.RecvStr = make(chan string)
+	if !tc.Connect() {
+		return
+	}
+	var line string
+	var res IMResponse
 	for {
 
-		fmt.Println(string(sms))
-		conn.Write(sms)
-		buf := make([]byte, 1024)
-		c, err := conn.Read(buf)
-		if err != nil {
-			fmt.Println("读取服务器数据异常:", err.Error())
-		}
-		msg:=string(buf[0:c])
-		fmt.Println(msg)
-		if msg!=""{
-			var res IMResponse
-			json.Unmarshal(buf[0:c],&res)
-			fmt.Println(res.Refer)
-			fmt.Println(res.Data["conn"]["key"])
-			switch res.Refer{
-				case "GET_KEY_RETURN":
-				sms=[]byte("{\"command\":\"GET_CONN\",\"data\":{\"user\":{\"id\":\"11\",\"token\":\"3233\",\"key\":\""+res.Data["conn"]["key"].(string)+"\"}}}")
-				conn.Write(sms)
-				case "GET_CONN_RETURN":
+		go Work(&tc)
+		fmt.Printf("发送:%s\n", line)
+		tc.SendStr <- line
+		s := <-tc.RecvStr
+		fmt.Printf("返回:%s\n", s)
+		if tc.Connect() && s != "" {
+			json.Unmarshal([]byte(s), &res)
+			switch res.Refer {
+			case "GET_KEY_RETURN":
+				//建立连接
+				line = "{\"command\":\"GET_CONN\",\"data\":{\"user\":{\"id\":\"11\",\"token\":\"11\",\"key\":\"" + res.Data["conn"]["key"].(string) + "\"}}}"
+			case "GET_CONN_RETURN":
+				//创建会话
+				line = "{\"command\":\"CREATE_SESSION\",\"data\":{\"session\":{\"sender\":\"11\",\"receiver\":\"22\",\"token\":\"11\"}}}"
 
-				case "CREATE_SESSION_RETURN":
+			case "CREATE_SESSION_RETURN":
+				//发送消息session
+				line = "{\"command\":\"SEND_MSG\",\"data\":{\"message\":{\"content\":\"Hello  World\",\"ticket\":\"" + res.Data["session"]["ticket"].(string) + "\",\"token\":\"11\"}}}"
 
 			}
 
 		}
 
 	}
-
 }
